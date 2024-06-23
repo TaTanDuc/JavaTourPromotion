146efe19008ade7362b16769d1a9e47a94c453eb
TungIT2912 <126306375+TungIT2912@users.noreply.github.com>
2024-06-23 13:42:15 +0700

    To nham To lai

--- src/main/java/com/team12/JavaTourPromotion/controller/CityController.java
+++ src/main/java/com/team12/JavaTourPromotion/controller/CityController.java
@@ -4,7 +4,6 @@ import com.team12.JavaTourPromotion.model.Cities;
 import com.team12.JavaTourPromotion.service.CityService;
 import com.team12.JavaTourPromotion.service.ProviceService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,35 +17,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 public class CityController {
 
-    private final ProductRepository productRepository;
-    // Retrieve all products from the database
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    @Autowired
+    private ProviceService provinceService;
+    @Autowired
+    private CityService cityService; // Đảm bảo bạn đã inject CategoryService
+    // Display a list of all products
+    @GetMapping("/cities")
+    public String showCityList(Model model) {
+        model.addAttribute("cities", cityService.getAllCities());
+        return "/cities/city-list";
     }
-    // Retrieve a product by its id
-    public Optional<Product> getProductById(Long id) {
-        return productRepository.findById(id);
-    }
-    // Add a new product to the database
-    public Product addProduct(Product product) {
-        return productRepository.save(product);
-    }
-    // Update an existing product
-    public Product updateProduct(@NotNull Product product) {
-        Product existingProduct = productRepository.findById(product.getId())
-                .orElseThrow(() -> new IllegalStateException("Product with ID " +
-                        product.getId() + " does not exist."));
-        existingProduct.setName(product.getName());
-        existingProduct.setPrice(product.getPrice());
-        existingProduct.setDescription(product.getDescription());
-        existingProduct.setCategory(product.getCategory());
-        return productRepository.save(existingProduct);
-    }
-    // Delete a product by its id
-    public void deleteProductById(Long id) {
-        if (!productRepository.existsById(id)) {
-            throw new IllegalStateException("Product with ID " + id + " does not exist.");
-        }
-        productRepository.deleteById(id);
-    }
+    // For adding a new product
+
+    // For editing a product
+
 }
