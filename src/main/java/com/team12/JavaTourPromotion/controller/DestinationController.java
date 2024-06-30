26b61ecdb4669831ca5401c5db3710d5da66d32a
TLong_1512 <125524778+ThanhLong1512@users.noreply.github.com>
2024-06-30 11:58:42 +0700

    Done-1

--- src/main/java/com/team12/JavaTourPromotion/controller/DestinationController.java
+++ src/main/java/com/team12/JavaTourPromotion/controller/DestinationController.java
@@ -3,6 +3,7 @@ package com.team12.JavaTourPromotion.controller;
 import com.team12.JavaTourPromotion.service.DestinationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -28,145 +29,31 @@ import java.util.Optional;
 public class DestinationController {
     @Autowired
     private final DestinationService destinationService;
-
-    @Autowired
-    private DestinationService destinationService;
-    @Autowired
-    private CategoryService categoryService;
-    @Autowired
-    private DistrictsOrWardsService districtsOrWardsService;
-    @Autowired
-    private CityService cityService; // Đảm bảo bạn đã inject CategoryService
-    @Autowired
-    private ProviceService proviceService;
     @GetMapping({"/", "/destinations"})
     public String showDestinationList(Model model) {
         model.addAttribute("destination", destinationService.getAllDestination());
         return "Homepage/Home";
     }
-    // For adding a new product
-    @GetMapping("/add")
-    public String showAddForm(Model model) {
-        model.addAttribute("destinations", new Destinations());
-        model.addAttribute("categories", categoryService.getAlCatologies());
-        model.addAttribute("provinces", proviceService.getAllProvinces());
-        model.addAttribute("cities", cityService.getAllCities());
-        model.addAttribute("dows", districtsOrWardsService.getAllDistrictsOrWards());//Load categories
-        return "/destinations/add-destination";
+    @GetMapping("/About")
+    public String showInformationAbout(Model model) {
+        return "Homepage/About";
     }
-    @PostMapping("/destinations/add")
-    public String addDestination(@Valid Destinations destination, BindingResult result,
-                             @RequestParam("ImageUrl") MultipartFile imageFile,
-                             @RequestParam("images") List<MultipartFile> imageFiles) {
-        if (result.hasErrors()) {
-            return "/destinations/add-destination";
-        }
-
-        // Upload image file
-        if (!imageFile.isEmpty()) {
-            try {
-                byte[] bytes = imageFile.getBytes();
-                String fileName = StringUtils.cleanPath(imageFile.getOriginalFilename());
-                destination.setImageUrl("/images/" + fileName); // Save the image URL
-                Path path = Paths.get("src/main/resources/static/images/" + fileName);
-                Files.write(path, bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        List<DestinationImages> destinationImages = new ArrayList<>();
-        for (MultipartFile file : imageFiles) {
-            if (!file.isEmpty()) {
-                try {
-                    byte[] bytes = file.getBytes();
-                    String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-                    Path path = Paths.get("src/main/resources/static/images/" + fileName);
-                    Files.write(path, bytes);
-
-                    DestinationImages destinationImage = new DestinationImages();
-                    destinationImage.setPath("/images/" + fileName);
-                    destinationImage.setDestination(destination);
-                    destinationImages.add(destinationImage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        destination.setImages(destinationImages);
-
-        destinationService.addDestination(destination);
-        return "redirect:/destinations";
+    @GetMapping("/Contact")
+    public String showContact(Model model) {
+        return "Homepage/Contact";
     }
-    // For editing a product
-    @GetMapping("/destinations/edit/{id}")
-    public String showEditForm(@PathVariable Long id, Model model) {
-        Destinations destination = destinationService.getDestinationById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid destination Id:" + id));
-        model.addAttribute("destinations", destination);
-        model.addAttribute("categories", categoryService.getAlCatologies());
-        model.addAttribute("provinces", proviceService.getAllProvinces());
-        model.addAttribute("cities", cityService.getAllCities());
-        model.addAttribute("dows", districtsOrWardsService.getAllDistrictsOrWards());
-        return "/destinations/update-destination";
-    }
-    @PostMapping("/destinations/update/{id}")
-    public String updateProduct(@PathVariable Long id, @Valid Destinations destination,
-                                BindingResult result,
-                                @RequestParam("ImageUrl") MultipartFile imageFile,
-                                @RequestParam("images") List<MultipartFile> imageFiles) {
-        if (result.hasErrors()) {
-            destination.setId(id);
-            return "/destinations/update-destination";
-        }
-        Destinations existingDestination = destinationService.getDestinationById(destination.getId())
-                .orElseThrow(() -> new IllegalStateException("Destination with ID " +
-                        destination.getId() + " does not exist."));
-        // Upload image file
-        if (!imageFile.isEmpty()) {
-            existingDestination.setImageUrl(destination.getImageUrl());
-        }
-        else{
-            try {
-                byte[] bytes = imageFile.getBytes();
-                String fileName = StringUtils.cleanPath(imageFile.getOriginalFilename());
-                existingDestination.setImageUrl("/images/" + fileName); // Save the image URL
-                Path path = Paths.get("src/main/resources/static/images/" + fileName);
-                Files.write(path, bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        List<DestinationImages> destinationImages = new ArrayList<>();
-        for (MultipartFile file : imageFiles) {
-            if (!file.isEmpty()) {
-                try {
-                    byte[] bytes = file.getBytes();
-                    String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-                    Path path = Paths.get("src/main/resources/static/images/" + fileName);
-                    Files.write(path, bytes);
-
-                    DestinationImages destinationImage = new DestinationImages();
-                    destinationImage.setPath("/images/" + fileName);
-                    destinationImage.setDestination(destination);
-                    destinationImages.add(destinationImage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            else {
-                existingDestination.setImages(destinationImages);
-            }
-        }
-        existingDestination.setImages(destinationImages);
-
-        destinationService.updateDestination(destination);
-        return "redirect:/destinations";
-    }
-    // Handle request to delete a product
-    @GetMapping("/destinations/delete/{id}")
-    public String deleteProduct(@PathVariable Long id) {
-        destinationService.deleteDestinationById(id);
-        return "redirect:/destinations";
-    }
-
+    @GetMapping("/Login")
+    public String showLogin(Model model) {
+        return "Login/login";
+    }   
+//     @GetMapping("/books")
+// //  public ResponseEntity<List<DES>> getAllBooks(Integer pageNo, Integer pageSize, String sortBy) {
+// //  return ResponseEntity.ok(destinationService.getAllDestination(
+// //  pageNo == null ? 0 : pageNo,
+// //  pageSize == null ? 20 : pageSize,
+// //  sortBy == null ? "id" : sortBy)
+// //  .stream()
+// //  .map(BookGetVm::from)
+// //  .toList());
+// //  }
 }
